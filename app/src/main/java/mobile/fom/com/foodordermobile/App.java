040e0e88e1eafaeb4df668c47f4590b43f7ebef1
Administrator <1421589053@qq.com>
2019-04-25 17:26:03 +0800

    nothing

--- app/src/main/java/mobile/fom/com/foodordermobile/App.java
+++ app/src/main/java/mobile/fom/com/foodordermobile/App.java
@@ -11,6 +11,7 @@ public class App extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         context = getApplicationContext();
     }
 
